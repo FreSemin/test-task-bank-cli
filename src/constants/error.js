3c7ff3eb553d7556d8ffea5f8d4cbc256aa535a6
fresemin <fresemin@gmail.com>
2024-02-27 22:24:03 +0300

    feat: argument not valid format error constant

--- src/constants/error.js
+++ src/constants/error.js
@@ -2,3 +2,5 @@ export const TEXT_ERROR = 'Error: ';
 export const NOT_EXISTING_COMMAND_ERROR = (command) => `Command: ${command} doesn't exist!`;
 
 export const NOT_EXISTING_OPERATION_ERROR = (operation, command) => `Operation: ${operation} doesn't exist for ${command}!`;
+
+export const ARGUMENT_NOT_VALID_FORMAT = (argument) => `Argument ${argument} is wrong format!`;
