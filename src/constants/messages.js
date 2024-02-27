25a21a256d1b7dc68ce1fadeffd7e3a41369d5e4
fresemin <fresemin@gmail.com>
2024-02-27 22:00:55 +0300

    feat: enter command message constant

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,6 +1,8 @@
 export const WELCOME_USER_TEXT = 'Welcome to Bank CLI!';
 export const LIST_ALL_COMMANDS_TEXT = 'You can get full list of available commands by typing "list"';
 
+export const ENTER_COMMAND_TEXT = 'Enter your command.';
+
 export const EXIT_USER_TEXT = 'Thank yor for using BANK CLI, goodbye!';
 
 export const NO_BANKS_TEXT = 'No any banks!';
