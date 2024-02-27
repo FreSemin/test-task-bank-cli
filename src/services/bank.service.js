0a298697d7e0b709f50f17e434b77e6ec18f15ea
fresemin <fresemin@gmail.com>
2024-02-27 20:43:01 +0300

    feat: basic bank service logic

--- src/services/bank.service.js
+++ src/services/bank.service.js
@@ -1,7 +1,32 @@
+import { NOT_EXISTING_OPERATION_ERROR } from '../constants/index.js';
+import { commands } from '../models/index.js';
+
 export class BankService {
-  constructor() { }
+  #bankAvailableOperations = null;
+
+  constructor() {
+    this.#bankAvailableOperations = commands.bank.availableOperations;
+  }
+
+  #onGetInfo(infoArgument) {
+    if (infoArgument === this.#bankAvailableOperations.info.arguments.all.name) {
+      console.log('get all');
+    } else {
+      console.log('get one');
+    }
+  }
 
   handleOperation(lineArguments) {
-    console.log('lineArguments: ', lineArguments);
+    switch (lineArguments[0]) {
+      case commands.bank.availableOperations.info.name: {
+        this.#onGetInfo(lineArguments[1]);
+        break;
+      }
+
+
+      default: {
+        throw new Error(NOT_EXISTING_OPERATION_ERROR(lineArguments[0], commands.bank.command));
+      }
+    }
   }
 }
