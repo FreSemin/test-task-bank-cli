76deafdfabf7e30fd2c652b479172f5147b8c97b
fresemin <fresemin@gmail.com>
2024-02-27 17:59:32 +0300

    feat: bank command info

--- src/models/commands.models.js
+++ src/models/commands.models.js
@@ -1,4 +1,27 @@
 export const commands = {
+  bank: {
+    command: 'bank',
+    description: 'Perform operations on Bank',
+    example: 'bank <operation_name> <operation_arguments>',
+    availableOperations: [
+      {
+        name: 'info',
+        description: 'Log information about banks',
+        arguments: [
+          {
+            name: 'all',
+            description: 'Log information about all banks!',
+            example: 'bank info all',
+          },
+          {
+            name: '<bank_id>',
+            description: 'Log information about one Bank by id!',
+            example: 'bank info 13',
+          }
+        ]
+      }
+    ],
+  },
   list: {
     command: 'list',
     description: 'Log list of all available CLI commands!',
