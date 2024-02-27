ecedaacbe012579c8e22573720c2d47764803009
fresemin <fresemin@gmail.com>
2024-02-27 21:53:35 +0300

    feat: no banks message constant

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -2,3 +2,5 @@ export const WELCOME_USER_TEXT = 'Welcome to Bank CLI!';
 export const LIST_ALL_COMMANDS_TEXT = 'You can get full list of available commands by typing "list"';
 
 export const EXIT_USER_TEXT = 'Thank yor for using BANK CLI, goodbye!';
+
+export const NO_BANKS_TEXT = 'No any banks!';
