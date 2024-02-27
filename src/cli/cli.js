369928b923b8357ee6d4de966c0712d017f4e117
fresemin <fresemin@gmail.com>
2024-02-27 15:11:02 +0300

    ref: cli add init read line method

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,9 +1,21 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
 export default class CLI {
+  #readLine = null;
+
   constructor() {
-    this.start();
+
+    this.#initReadLine();
+
+    this.#start();
   }
 
-  start() {
+  #initReadLine() {
+    this.#readLine = readline.createInterface({ input, output });
+  }
+
+  #start() {
     console.log('CLI started!');
   }
 }
