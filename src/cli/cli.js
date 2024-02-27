06cad0cad9d462c803601c61e3e96a7abc8a9266
fresemin <fresemin@gmail.com>
2024-02-27 15:12:14 +0300

    feat: init commands service

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,10 +1,14 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
+import { CommandsService } from '../services/index.js';
 
 export default class CLI {
   #readLine = null;
 
+  #commandsService = null;
+
   constructor() {
+    this.#commandsService = new CommandsService();
 
     this.#initReadLine();
 
