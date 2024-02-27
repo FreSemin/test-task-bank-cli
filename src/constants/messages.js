00ebe19a03e4a0597aef04a80abb90d4f7f988ab
fresemin <fresemin@gmail.com>
2024-02-28 01:55:15 +0300

    feat: entity was deleted message constant

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -6,3 +6,6 @@ export const ENTER_COMMAND_TEXT = 'Enter your command.';
 export const EXIT_USER_TEXT = 'Thank yor for using BANK CLI, goodbye!';
 
 export const NO_BANKS_TEXT = 'No any banks!';
+
+export const ENTITY_WAS_DELETED = (entityName, propertyName, propertyValue) =>
+  `Entity ${entityName} with ${propertyName} = ${propertyValue} was deleted!`;
