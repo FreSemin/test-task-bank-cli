dd72371a35fff14ca48f947d2c5579d1231a6c2e
fresemin <fresemin@gmail.com>
2024-02-27 16:05:24 +0300

    feat: commands model

---
+++ src/models/commands.models.js
@@ -0,0 +1,12 @@
+export const commands = {
+  list: {
+    command: 'list',
+    description: 'Log list of all available CLI commands!',
+    example: 'list',
+  },
+  exit: {
+    command: 'exit',
+    description: 'End current session and close terminal!',
+    example: 'exit'
+  }
+};
