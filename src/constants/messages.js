80554b4732d87730d1f50b641d76a0f9d0e1efff
fresemin <fresemin@gmail.com>
2024-02-27 16:22:07 +0300

    feat: exit user text message

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,2 +1,4 @@
 export const WELCOME_USER_TEXT = 'Welcome to Bank CLI!';
 export const LIST_ALL_COMMANDS_TEXT = 'You can get full list of available commands by typing "list"';
+
+export const EXIT_USER_TEXT = 'Thank yor for using BANK CLI, goodbye!';
