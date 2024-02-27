5c093cc3d8ca396d2b552d9c8824d58ca4621ca3
fresemin <fresemin@gmail.com>
2024-02-27 20:42:40 +0300

    feat: not existing operation error constant

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,2 +1,4 @@
 export const TEXT_ERROR = 'Error: ';
 export const NOT_EXISTING_COMMAND_ERROR = (command) => `Command: ${command} doesn't exist!`;
+
+export const NOT_EXISTING_OPERATION_ERROR = (operation, command) => `Operation: ${operation} doesn't exist for ${command}!`;
