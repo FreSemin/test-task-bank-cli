0343c6f57698a88c1d58a3a7089813a3e1e16fd4
fresemin <fresemin@gmail.com>
2024-02-27 18:06:08 +0300

    ref: commands service using utils to log commands info

--- src/services/commands.service.js
+++ src/services/commands.service.js
@@ -1,59 +1,17 @@
-import {
-  COMMAND_ARGUMENT_LOG,
-  COMMAND_LOG,
-  COMMAND_OPERATION_LOG,
-  DESCRIPTION_LOG,
-  EXAMPLE_LOG,
-  NOT_EXISTING_COMMAND_ERROR
-} from '../constants/index.js';
+import { NOT_EXISTING_COMMAND_ERROR } from '../constants/index.js';
 import { commands } from '../models/index.js';
+import { logCommandsInfo } from '../utils/index.js';
 
 export class CommandsService {
-  constructor() { }
-
 
   isCloseCmd(line) {
     return line === commands.exit.command ? true : false;
   }
 
-  #logOperationArguments(operation) {
-    operation.arguments.forEach((argument) => {
-      console.group(COMMAND_ARGUMENT_LOG, argument.name);
-
-      console.log(DESCRIPTION_LOG, argument.description);
-      console.log(EXAMPLE_LOG, argument.example);
-
-      console.groupEnd();
-    });
-  }
-
-  #logAvailableOperations(command) {
-    if (command.availableOperations?.length > 0) {
-      command.availableOperations.forEach((operation) => {
-        console.group(COMMAND_OPERATION_LOG, operation.name);
-
-        console.log(DESCRIPTION_LOG, operation.description);
-
-        this.#logOperationArguments(operation);
-
-        console.groupEnd();
-      });
-    }
-  }
-
-  logAllCmds() {
+  logAllCommands() {
     const allCommands = Object.values(commands);
 
-    allCommands.forEach((command) => {
-      console.group(COMMAND_LOG, command.command);
-
-      console.log(DESCRIPTION_LOG, command.description);
-      console.log(EXAMPLE_LOG, command.example);
-
-      this.#logAvailableOperations(command);
-
-      console.groupEnd();
-    });
+    logCommandsInfo(allCommands);
   }
 
   handleCmd(line) {
@@ -63,7 +21,7 @@ export class CommandsService {
 
     switch (userCmd) {
       case commands.list.command: {
-        this.logAllCmds();
+        this.logAllCommands();
         break;
       }
 
